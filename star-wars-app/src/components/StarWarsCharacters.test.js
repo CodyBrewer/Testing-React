5f29f2fae1a0a54bab6629974e441626e23cd0d7
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-12 21:03:44 -0700

    test should render previous button & should render next button added

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -7,4 +7,14 @@ describe("Star Wars Component", () => {
   test("should render without Crashing", () => {
     render(<StarWarsCharacters />);
   });
+  test("should render previous button", () => {
+    const { getByText } = render(<StarWarsCharacters />);
+    const prevButton = getByText("Previous");
+    expect(prevButton).toBeInTheDocument();
+  });
+  test("should render next button", () => {
+    const { getByText } = render(<StarWarsCharacters />);
+    const nextButton = getByText("Next");
+    expect(nextButton).toBeInTheDocument();
+  });
 });
